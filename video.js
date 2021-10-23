e248fa3bc7cb5c79302a4f78e5629bfac6ba4723
rigo0523 <limon1285@gmail.com>
2021-10-22 23:16:40 -0500

    load event added

--- video.js
+++ video.js
@@ -1,9 +1,18 @@
-const videoPlayer = document.getElementById("video-player");
-const video = videoPlayer.querySelector(".video");
+const videoPlayer = document.getElementById("video-player"); //container
+const video = videoPlayer.querySelector(".video"); //video element
+const videoSource = videoPlayer.querySelector(".videoSource");
 const playButton = videoPlayer.querySelector(".play-button");
+const body = document.getElementsByTagName("body")[0];
 
 //volume
 const volume = videoPlayer.querySelector(".volume");
+
+//load page / video
+window.addEventListener("load", (e) => {
+  video.src =
+    "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4";
+  video.load();
+});
 
 //play and Pause Button
 playButton.addEventListener("click", (e) => {
